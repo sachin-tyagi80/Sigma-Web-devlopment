653a71d6c47b200e913205166c9d951355417ec8
sachin-tyagi80 <sachintyagi6322@gmail.com>
2026-01-07 23:08:20 +0530

    export

---
+++ Backend/Math.js
@@ -0,0 +1,21 @@
+const sum = (a, b) => a + b;
+const mul = (a, b) => a * b;
+const g = 9.8;
+const PI = 3.14;
+///1st way
+// let obj = {
+//   sum: sum,
+//   mul: mul,
+//   g: g,
+//   PI: PI,
+// };
+// module.exports = obj;
+
+///2nd way
+// module.exports = {
+//   sum: sum,
+//   mul: mul,
+//   g: g,
+//   PI: PI,
+// };
+//module.exports = 123;
---
+++ Backend/script.js
@@ -0,0 +1,14 @@
+// let n = 5;
+
+// for (let i = 0; i < n; i++) {
+//   console.log("hello,", i);
+// }
+
+//console.log(process.argv);
+
+// const someValue = require("./Math");
+// //console.log(someValue);
+// console.log(someValue.sum(2, 2));
+
+const info = require("./Fruits");
+console.log(info);
